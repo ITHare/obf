283e2f8ed8ca264b32a7de3e6b54639c4e6303be
NoBugsHare <nobugs@ithare.com>
2018-01-23 05:57:31 +0100

    fixes to test/interal.cpp

--- test/internal.cpp
+++ test/internal.cpp
@@ -216,15 +216,17 @@ int main(int argc, char** argv) {
 	//obf_dbgPrint();
 	//std::string s = obf_literal<decltype(""), "",0, 1>();
 	//std::string s = deobfuscate<seed,cycles>(constexpr obfuscate<seed,XYZ>("Long string which makes lots of sense"));
-	uint8_t user_key[CHACHA_KEY_SIZE] = { 0 };
-	uint8_t iv[CHACHA_CTR_SIZE] = { 1,0 };
-	EVP_CIPHER_CTX ctx;
-	int ok = chacha_init_key(&ctx, user_key, iv, 1);
-	
-	uint8_t inp[16] = {0};
-	uint8_t out[16] = {0};
-	ok = chacha_cipher(&ctx, out, inp, 16);
-	
+	uint8_t user_key[ CHACHA_KEY_SIZE] = { 0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x1a,0x1b,0x1c,0x1d,0x1e,0x1f };
+	uint8_t iv[ CHACHA_CTR_SIZE] = { 1,0,0,0,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x4a,0x00,0x00,0x00,0x00 };//1st 4 bytes are ctr
+	 EVP_CHACHA chacha(user_key, iv, 1);
+
+	uint8_t inp[16] = { 0 };
+	uint8_t out[16] = { 0 };
+	OBF_CALL3(chacha.cipher)(out, inp, 16);
+	uint8_t expected_out[16] = { 0x22,0x4f,0x51,0xf3,0x40,0x1b,0xd9,0xe1,0x2f,0xde,0x27,0x6f,0xb8,0x63,0x1d,0xed };
+	//from RFC7539
+	assert(std::equal(std::begin(out), std::end(out), std::begin(expected_out), std::end(expected_out)));
+
 	//for(int i=0;i<16;++i) {
 	//	std::cout << std::hex << int(out[i]) << " " << std::endl;
 	//}
