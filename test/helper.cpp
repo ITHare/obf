aa9d944a663abb8d2cc78fb1475acb60b4db5b95
NoBugsHare <nobugs@ithare.com>
2018-01-23 06:34:52 +0100

    MSVC random testing: warnings as errors

--- test/helper.cpp
+++ test/helper.cpp
@@ -70,12 +70,12 @@ std::string replace_string(std::string subject, std::string search,//adapted fro
 }
 std::string buildRelease(std::string defines_) {
 	std::string defines = replace_string(defines_, " -D", " /D");
-	return std::string("cl /permissive- /GS /GL /W3 /Gy /Zc:wchar_t /Gm- /O2 /sdl /Zc:inline /fp:precise /DNDEBUG /D_CONSOLE /D_UNICODE /DUNICODE /errorReport:prompt /WX- /Zc:forScope /GR- /Gd /Oi /MT /EHsc /nologo /diagnostics:classic /std:c++17 /cgthreads1") + defines + " ..\\official.cpp";
+	return std::string("cl /permissive- /GS /GL /W3 /Gy /Zc:wchar_t /Gm- /O2 /sdl /Zc:inline /fp:precise /DNDEBUG /D_CONSOLE /D_UNICODE /DUNICODE /errorReport:prompt /WX /Zc:forScope /GR- /Gd /Oi /MT /EHsc /nologo /diagnostics:classic /std:c++17 /cgthreads1") + defines + " ..\\official.cpp";
 		//string is copy-pasted from Rel-NoPDB config
 }
 std::string buildDebug(std::string defines_) {
 	std::string defines = replace_string(defines_, " -D", " /D");
-	return std::string("cl /permissive- /GS /W3 /Zc:wchar_t /ZI /Gm /Od /sdl /Zc:inline /fp:precise /D_DEBUG /D_CONSOLE /D_UNICODE /DUNICODE /errorReport:prompt /WX- /Zc:forScope /RTC1 /Gd /MDd /EHsc /nologo /diagnostics:classic /std:c++17 /cgthreads1") + defines + " ..\\official.cpp";
+	return std::string("cl /permissive- /GS /W3 /Zc:wchar_t /ZI /Gm /Od /sdl /Zc:inline /fp:precise /D_DEBUG /D_CONSOLE /D_UNICODE /DUNICODE /errorReport:prompt /WX /Zc:forScope /RTC1 /Gd /MDd /EHsc /nologo /diagnostics:classic /std:c++17 /cgthreads1") + defines + " ..\\official.cpp";
 		//string is copy-pasted from Debug config
 }
 std::string genRandom64() {
