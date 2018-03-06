00e501730eabd2fb50d4d88c04ad349fff06ae34
No Bugs' Hare <nobugs@ithare.com>
2018-03-06 14:28:09 +0100

    making GCC happy, cleaning up artifacts

--- test/obftest.cpp
+++ test/obftest.cpp
@@ -67,30 +67,6 @@ ITHARE_OBF_NOINLINE OBFI6(uint64_t) factorial(OBFI6(int64_t) x) {
 }
 
 /*
-ITHARE_OBF_NOINLINE void test_chacha_cipher0(const uint8_t* inp, uint8_t* out) {
-	uint8_t user_key[ITOBF_TLS CHACHA_KEY_SIZE] = { 0 };
-	uint8_t iv[ITOBF_TLS CHACHA_CTR_SIZE] = { 0 };
-	ITOBF_TLS EVP_CHACHA<> chacha(user_key, iv, 1);
-
-	chacha.cipher(out, inp, 16);
-}
-
-ITHARE_OBF_NOINLINE void test_chacha_cipher1(const uint8_t* inp, uint8_t* out) {
-	uint8_t user_key[ITOBF_TLS CHACHA_KEY_SIZE] = { 1, 0 };
-	uint8_t iv[ITOBF_TLS CHACHA_CTR_SIZE] = { 0 };
-	ITHARE_OBF_OBFLIBCLASS(ITOBF_TLS EVP_CHACHA) chacha(user_key, iv, 1);
-
-	OBF_CALL3(chacha.cipher)(out, inp, 16);
-}
-
-ITHARE_OBF_NOINLINE void test_chacha_cipher_rfc(const uint8_t* inp, uint8_t* out) {
-	uint8_t user_key[ITOBF_TLS CHACHA_KEY_SIZE] = { 0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x1a,0x1b,0x1c,0x1d,0x1e,0x1f };
-	uint8_t iv[ITOBF_TLS CHACHA_CTR_SIZE] = { 1,0,0,0,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x4a,0x00,0x00,0x00,0x00 };//1st 4 bytes are ctr
-	ITHARE_OBF_OBFLIBCLASS(ITOBF_TLS EVP_CHACHA) chacha(user_key, iv, 1);
-
-	OBF_CALL4(chacha.cipher)(out, inp, 16);
-}
-
 class Benchmark {
 	std::chrono::high_resolution_clock::time_point start;
 
@@ -121,7 +97,7 @@ public:
 #endif
 
 const lest::test module[] = {
-	CASE("obf::factorial()") {
+	CASE("obf::factorial()",) {
 		auto f = factorial(17); ITHARE_OBF_DBGPRINT(f);
 		EXPECT(f==UINT64_C(355687428096000));
 		EXPECT( factorial(18) == UINT64_C(6402373705728000));
@@ -129,45 +105,7 @@ const lest::test module[] = {
 		EXPECT( factorial(20) == UINT64_C(2432902008176640000));
 		EXPECT( factorial(21) == UINT64_C(14197454024290336768));//with wrap-around(!)
 	},
-	/*CASE("chacha20(key=0...0,nonce=0...0)") {
-		uint8_t inp[16] = { 0 };
-		uint8_t out[16] = { 0 };
-
-		test_chacha_cipher0(inp, out);
-		uint8_t expected_out[16] = { 0x76, 0xb8, 0xe0, 0xad, 0xa0, 0xf1, 0x3d, 0x90, 0x40, 0x5d, 0x6a, 0xe5, 0x53, 0x86, 0xbd, 0x28 };
-			//from https://github.com/secworks/chacha_testvectors/blob/master/src/chacha_testvectors.txt (look for SECOND entry with "Rounds: 20" in TC1, SECOND entry corresponds to 256-bit Chacha20 which is used in TLS per RFC7539
-		EXPECT(std::equal(std::begin(out), std::end(out), std::begin(expected_out), std::end(expected_out)));
-	},
-	CASE("chacha20(key=10...0,nonce=0...0)") {
-		uint8_t inp[16] = {0};
-		uint8_t out[16] = {0};
-		test_chacha_cipher1(inp, out);
-		uint8_t expected_out[16] = { 0xc5, 0xd3, 0x0a, 0x7c, 0xe1, 0xec, 0x11, 0x93, 0x78, 0xc8, 0x4f, 0x48, 0x7d, 0x77, 0x5a, 0x85 };
-			//from https://github.com/secworks/chacha_testvectors/blob/master/src/chacha_testvectors.txt (look for SECOND entry with "Rounds: 20" in TC2, SECOND entry corresponds to 256-bit Chacha20 which is used in TLS per RFC7539
-		EXPECT(std::equal(std::begin(out), std::end(out), std::begin(expected_out), std::end(expected_out)));
-	},
-	CASE("chacha20(RFC7539)") {
-		
-		uint8_t inp[16] = {0};
-		uint8_t out[16] = {0};
-		test_chacha_cipher_rfc(inp, out);
-		uint8_t expected_out[16] = { 0x22,0x4f,0x51,0xf3,0x40,0x1b,0xd9,0xe1,0x2f,0xde,0x27,0x6f,0xb8,0x63,0x1d,0xed };
-			//from RFC7539
-		EXPECT(std::equal(std::begin(out), std::end(out), std::begin(expected_out), std::end(expected_out)));
-	},
-	CASE("chacha20(RFC7539, fully compile-time)") {
-		constexpr uint8_t user_key[ITOBF_TLS CHACHA_KEY_SIZE] = { 0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x1a,0x1b,0x1c,0x1d,0x1e,0x1f };
-		constexpr uint8_t iv[ITOBF_TLS CHACHA_CTR_SIZE] = { 1,0,0,0,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x4a,0x00,0x00,0x00,0x00 };//1st 4 bytes are ctr
-		constexpr ITOBF_TLS EVP_CHACHA<> chacha = ITHARE_OBF_CALL_AS_CONSTEXPR(ITOBF_TLS EVP_CHACHA<>::construct)(user_key, iv, 1);
-		
-		constexpr uint8_t inp[16] = {0};
-		constexpr std::pair<ITOBF_TLS EVP_CHACHA<>,ITOBF ObfArrayWrapper<unsigned char,16>> ciphered = chacha.constexpr_cipher(inp);
-		constexpr ITOBF_TLS EVP_CHACHA<> chacha2 = ciphered.first;
-		constexpr ITOBF ObfArrayWrapper<unsigned char, 16> out = ciphered.second;
-		uint8_t expected_out[16] = { 0x22,0x4f,0x51,0xf3,0x40,0x1b,0xd9,0xe1,0x2f,0xde,0x27,0x6f,0xb8,0x63,0x1d,0xed };
-			//from RFC7539
-		EXPECT(std::equal(std::begin(out.arr), std::end(out.arr), std::begin(expected_out), std::end(expected_out)));
-	},
+	/*
 	CASE("benchmarks") {
 #if !defined(ITHARE_OBF_ENABLE_AUTO_DBGPRINT) || ITHARE_OBF_ENABLE_AUTO_DBGPRINT == 2//excluding platform-specific stuff to avoid spurious changes to obftemp.txt with -DITHARE_OBF_ENABLE_AUTO_DBGPRINT
 		{
