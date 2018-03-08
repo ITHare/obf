e443e225a552c57471f214230610acc3a96b8091
No Bugs' Hare <nobugs@ithare.com>
2018-03-08 08:47:38 +0100

    separated testing for kscope-with-obfs, and obf-specific one

--- test/randomtestgen.cpp
+++ test/randomtestgen.cpp
@@ -37,25 +37,110 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 static const char* obf_randomtest_files[] = { "../obftest.cpp", nullptr };
 
+inline bool starts_with(std::string a,std::string b) {
+	return a.compare(0,b.length(),b) == 0;
+}
+
 class ObfTestEnvironment : public KscopeTestEnvironment {
 	public:
 	virtual std::string test_src_dir() override { return  src_dir_prefix + "../../../kscope/test/"; }
-	virtual std::string file_list() override { return KscopeTestEnvironment::file_list() + make_file_list(obf_randomtest_files,src_dir_prefix); }
-	
-	virtual std::string always_define() override {
-	//path is relative to kscope/test
+	//virtual std::string file_list() override { return KscopeTestEnvironment::file_list() + make_file_list(obf_randomtest_files,src_dir_prefix); }
+
+	std::string obf_define(std::string s) {
+		if(starts_with(s,"SEED=")
+		   ||starts_with(s,"SEED2=")
+		   ||s=="CONSISTENT_XPLATFORM_IMPLICIT_SEEDS"
+		   ||s=="CRYPTO_PRNG"
+		   ||s=="DBG_RUNTIME_CHECKS"
+		   ||s=="DBG_ENABLE_DBGPRINT"
+		   ||s=="ENABLE_AUTO_DBGPRINT"
+		   ||s=="COMPILE_TIME_TESTS"
+		   )
+			return "ITHARE_OBF_" + s;
+		else
+			return "";
+	}
 #ifdef __GNUC__ //includes clang
 #ifdef __apple_build_version__
-		return "-DITHARE_KSCOPE_TEST_EXTENSION=\"../../obf/src/kscope_extension_for_obf.h\"";//no -latomic needed or possible for Apple Clang
+	static constexpr const char* lopt_extra ="";//no -latomic needed or possible for Apple Clang
 #else
-		return "-DITHARE_KSCOPE_TEST_EXTENSION=\"../../obf/src/kscope_extension_for_obf.h\" -latomic";
+	static constexpr const char* lopt_extra = " -latomic";
 #endif
-#elif defined(_MSC_VER)
-		return "/DITHARE_KSCOPE_TEST_EXTENSION=\"../../obf/src/kscope_extension_for_obf.h\"";
-#else
-#error
-#endif
+
+	virtual MultiString build_release(MultiString defines,std::string opts) override {
+		std::string kscopedefs = "";
+		std::string obfdefs = "";
+		for(std::string s:defines) {
+			kscopedefs += " -DITHARE_KSCOPE_" + s;
+			if(obf_define(s)!="")
+				obfdefs += " -D" + obf_define(s);
+		}
+		std::string cpplist = KscopeTestEnvironment::file_list() + make_file_list(obf_randomtest_files,src_dir_prefix);
+		std::string objlist0 = replace_string(cpplist,".cpp",".o");
+		std::string objlist1 = replace_string(objlist0,test_src_dir(),"");
+		std::string objlist = replace_string(objlist1,"../","");
+		return MultiString{{
+			"$CXX -c" + compiler_options_release() + " -DITHARE_KSCOPE_TEST_EXTENSION=\"../../obf/src/kscope_extension_for_obf.h\"" + kscopedefs + opts + KscopeTestEnvironment::file_list(),
+			"$CXX -c" + compiler_options_release() + " -DITHARE_KSCOPE_TEST_EXTENSION=\"../../obf/src/kscope_extension_for_obf.h\"" + obfdefs    + opts + make_file_list(obf_randomtest_files,src_dir_prefix),
+			"$CXX" + linker_options_release() + lopt_extra + opts + objlist
+			}};
 	}
+	virtual MultiString build_debug(MultiString defines,std::string opts) override {
+		std::string kscopedefs = "";
+		std::string obfdefs = "";
+		for(std::string s:defines) {
+			kscopedefs += " -DITHARE_KSCOPE_" + s;
+			if(obf_define(s)!="")
+				obfdefs += " -D" + obf_define(s);
+		}
+		std::string cpplist = KscopeTestEnvironment::file_list() + make_file_list(obf_randomtest_files,src_dir_prefix);
+		std::string objlist0 = replace_string(cpplist,".cpp",".o");
+		std::string objlist1 = replace_string(objlist0,test_src_dir(),"");
+		std::string objlist = replace_string(objlist1,"../","");
+		return MultiString{{
+			"$CXX -c" + compiler_options_debug() + " -DITHARE_KSCOPE_TEST_EXTENSION=\"../../obf/src/kscope_extension_for_obf.h\"" + kscopedefs + opts + KscopeTestEnvironment::file_list(),
+			"$CXX -c" + compiler_options_debug() + " -DITHARE_KSCOPE_TEST_EXTENSION=\"../../obf/src/kscope_extension_for_obf.h\"" + obfdefs    + opts + make_file_list(obf_randomtest_files,src_dir_prefix),
+			"$CXX" + linker_options_debug() + lopt_extra + opts + objlist
+			}};
+	}
+#elif defined(_MSC_VER)
+	virtual MultiString build_release(MultiString defines,std::string opts) {
+		std::string kscopedefs = "";
+		std::string obfdefs = "";
+		for(std::string s:defines) {
+			kscopedefs += " /DITHARE_KSCOPE_" + s;
+			if(obf_define(s)!="")
+				obfdefs += " /D" + obf_define(s);
+		}
+		std::string cpplist = KscopeTestEnvironment::file_list() + make_file_list(obf_randomtest_files,src_dir_prefix);
+		std::string objlist0 = replace_string(cpplist,".cpp",".obj");
+		std::string objlist1 = replace_string(objlist0,test_src_dir(),"");
+		std::string objlist = replace_string(objlist1,"../","");
+		return MultiString{{
+			"cl /c" + compiler_options_release() + " /DITHARE_KSCOPE_TEST_EXTENSION=\"../../obf/src/kscope_extension_for_obf.h\"" + defs + opts + KscopeTestEnvironment::file_list(),
+			"cl /c" + compiler_options_release() + " /DITHARE_KSCOPE_TEST_EXTENSION=\"../../obf/src/kscope_extension_for_obf.h\"" + defs + opts + make_file_list(obf_randomtest_files,src_dir_prefix),
+			"link " + linker_options_release() + opts + objlist;
+			}};
+	}
+	virtual MultiString build_debug(MultiString defines,std::string opts) {
+		std::string kscopedefs = "";
+		std::string obfdefs = "";
+		for(std::string s:defines) {
+			kscopedefs += " /DITHARE_KSCOPE_" + s;
+			if(obf_define(s)!="")
+				obfdefs += " /D" + obf_define(s);
+		}
+		std::string cpplist = KscopeTestEnvironment::file_list() + make_file_list(obf_randomtest_files,src_dir_prefix);
+		std::string objlist0 = replace_string(cpplist,".cpp",".obj");
+		std::string objlist1 = replace_string(objlist0,test_src_dir(),"");
+		std::string objlist = replace_string(objlist1,"../","");
+		return MultiString{{
+			"cl /c" + compiler_options_debug() + " /DITHARE_KSCOPE_TEST_EXTENSION=\"../../obf/src/kscope_extension_for_obf.h\"" + defs + opts + KscopeTestEnvironment::file_list(),
+			"cl /c" + compiler_options_debug() + " /DITHARE_KSCOPE_TEST_EXTENSION=\"../../obf/src/kscope_extension_for_obf.h\"" + defs + opts + make_file_list(obf_randomtest_files,src_dir_prefix),
+			"link " + linker_options_debug() + opts + objlist;
+			}};
+	}
+#endif
 	
 #if defined(__APPLE_CC__) || defined(__linux__)
 	virtual std::string check_exe(int nseeds,config cfg,Flags flags) override {
